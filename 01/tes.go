b1fa35007a94afffcb2db477d1971e91adf6505a
Jeremy Jaynes <JeremyAJaynes@gmail.com>
2017-04-19 15:24:58 -0500

    Hello World
    Testing Go

---
+++ 01/tes.go
@@ -0,0 +1,4 @@
+package tes2
+
+var MyName = 15*5
+var Findme = 16
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/JJaynes/hello/01"
+)
+
+
+func main() {
+	fmt.Println("Hello, Playground")
+	fmt.Println(tes2.MyName)
+	fmt.Println(tes2.Findme)
+}
